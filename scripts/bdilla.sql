10dc6e166ec662847a7816351c380bd289b41ffc
bdilla <dillinghamb@gmail.com>
2020-08-20 14:34:05 -0500

    content_rating/price_range

--- scripts/bdilla.sql
+++ scripts/bdilla.sql
@@ -65,7 +65,7 @@ ORDER BY name*/
 		AND COALESCE(average_rating, app_rating, play_rating) > 4.6
 	ORDER BY name, has_both_stores DESC*/
 	
-SELECT DISTINCT ON (app_name) app_name,	play_name,
+/*SELECT DISTINCT ON (app_name) app_name,	play_name, subquery.a_content, subquery.p_content,
 	CASE WHEN subquery.has_app_store = TRUE AND subquery.has_play_store = TRUE THEN TRUE ELSE FALSE END AS has_both_stores,
 	/*CASE
 		WHEN average_rating is null AND app_rating is null THEN ROUND(play_rating, 2)
@@ -76,7 +76,7 @@ SELECT DISTINCT ON (app_name) app_name,	play_name,
 	ROUND(average_rating,2) AS average_rating, app_rating, play_rating, CAST(subquery.price AS float), CAST(subquery.app_review_count AS int), CAST(subquery.play_review_count AS int)
 	FROM
 	(SELECT
-	 	p.name as play_name, a.name as app_name,
+	 	p.name as play_name, a.name as app_name, a.content_rating AS a_content, p.content_rating AS p_content,
 	 	CASE WHEN a.rating is not null THEN TRUE ELSE FALSE END AS has_app_store,
 		CASE WHEN p.rating is not null THEN TRUE ELSE FALSE END AS has_play_store,	
 		CAST(a.price AS float), CAST(a.review_count AS int) AS app_review_count, CAST(p.review_count AS int) AS play_review_count, a.rating AS app_rating,  p.rating AS play_rating, ((a.rating + (ROUND(p.rating / .5, 0) * .5)) / 2) AS average_rating
@@ -85,12 +85,41 @@ SELECT DISTINCT ON (app_name) app_name,	play_name,
 		ON p.name = a.name
 		-- Filter for price
 		WHERE a.price < 1.01) as subquery		
-	-- Filter for review count over 1000 on either store	
+	-- Filter for review count over 5000 on either store	
 	WHERE (subquery.app_review_count > 5000
 		OR subquery.play_review_count > 5000)
 		AND COALESCE(average_rating, app_rating, play_rating) > 4.6
-	
+		AND play_name IS NOT NULL
+	ORDER BY app_name, has_both_stores DESC --, refined_rating*/	
+
+SELECT DISTINCT ON (app_name) app_name,	play_name, subquery.a_content, subquery.p_content,
+	CASE WHEN subquery.has_app_store = TRUE AND subquery.has_play_store = TRUE THEN TRUE ELSE FALSE END AS has_both_stores,
+	/*CASE
+		WHEN average_rating is null AND app_rating is null THEN ROUND(play_rating, 2)
+		WHEN average_rating is null AND play_rating is null THEN ROUND(app_rating, 2)
+		WHEN average_rating is not null THEN ROUND(average_rating, 2)
+		ELSE '0'
+		END AS refined_rating,*/
+	ROUND(average_rating,2) AS average_rating, app_rating, play_rating, CAST(subquery.price AS float), CAST(subquery.app_review_count AS int), CAST(subquery.play_review_count AS int)
+	FROM
+	(SELECT
+	 	p.name as play_name, a.name as app_name, a.content_rating AS a_content, p.content_rating AS p_content,
+	 	CASE WHEN a.rating is not null THEN TRUE ELSE FALSE END AS has_app_store,
+		CASE WHEN p.rating is not null THEN TRUE ELSE FALSE END AS has_play_store,	
+		CAST(a.price AS float), CAST(a.review_count AS int) AS app_review_count, CAST(p.review_count AS int) AS play_review_count, a.rating AS app_rating,  p.rating AS play_rating, ((a.rating + (ROUND(p.rating / .5, 0) * .5)) / 2) AS average_rating
+		FROM play_store_apps AS p	
+		FULL JOIN app_store_apps AS a	
+		ON p.name = a.name
+		-- Filter for price
+		WHERE a.price > 1.01 AND a.price <9.99) as subquery		
+	-- Filter for review count over 5000 on either store	
+	WHERE (subquery.app_review_count > 5000
+		OR subquery.play_review_count > 5000)
+		AND COALESCE(average_rating, app_rating, play_rating) > 4.0
+		AND play_name IS NOT NULL
 	ORDER BY app_name, has_both_stores DESC --, refined_rating
+
+	
 	
 
 	
